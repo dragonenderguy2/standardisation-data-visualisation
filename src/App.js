8c8a86ff4bed59a61ac9fdc0bbb2888034b3af9e
dragonenderguy2 <shaihalapre@hotmail.com>
2025-08-14 22:54:00 -0500

    feat: initial commit for src/App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div>
+            <h1>Bienvenue dans la Visualisation des Standards Ouverts</h1>
+            <p>Explorez différents standards ouverts dans divers domaines.</p>
+        </div>
+    );
+}
+
+export default App;
