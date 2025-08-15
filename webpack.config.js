47be9181221e758e0d511e548df49fe1a65ce61b
dragonenderguy2 <shaihalapre@hotmail.com>
2025-08-14 22:56:51 -0500

    Add comments for clarity

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,29 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: './src/index.js', // Point d'entrée principal de l'application
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'), // Répertoire de sortie
+        filename: 'bundle.js', // Nom du fichier de sortie
     },
     devServer: {
-        static: './dist',
+        static: './dist', // Dossier à servir pour le développement
     },
     module: {
         rules: [
             {
-                test: /\.js$/, // Regex pour tous les fichiers .js
+                test: \.js$, // Regex pour tous les fichiers .js
                 exclude: /node_modules/, // Exclure le dossier node_modules
                 use: {
-                    loader: 'babel-loader',
+                    loader: 'babel-loader', // Utilisation de babel-loader pour transpiler le code
                     options: {
-                        presets: ['@babel/preset-env', '@babel/preset-react'],
+                        presets: ['@babel/preset-env', '@babel/preset-react'], // Presets pour Babel
                     },
                 },
             },
         ],
     },
     resolve: {
-        extensions: ['.js', '.jsx'],
+        extensions: ['.js', '.jsx'], // Extensions à résoudre lors de l'importation
     },
 };
