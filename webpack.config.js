f38b15c3a14020253e4a20da6fda747341a36633
dragonenderguy2 <shaihalapre@hotmail.com>
2025-08-14 23:09:53 -0500

    Fix output path to handle empty output directory

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.js$/, // Regex for all .js files
+                test: /\\.js$/, // Regex for all .js files
                 exclude: /node_modules/, // Exclude the node_modules folder
                 use: {
                     loader: 'babel-loader', // Use babel-loader to transpile the code
